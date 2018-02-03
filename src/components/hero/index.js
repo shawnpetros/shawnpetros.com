b11a17be2511c8d280b04bfa1be63d353fc1dc85
Shawn Petros <arashcuzi@gmail.com>
2018-02-03 15:13:19 -0600

    starting refactor

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import Header from '../header-bar';
 import './index.css';
 
 export default ({ me, tagline, summary, company }) => (
   <div className="hero">
-    <div id="avatar"></div>
     <h1>{ me }</h1>
     <p className="lato tagline">{ tagline }</p>
     <p className="summary">{ summary }<a href={ company.link } className="nm">{ company.name }</a></p>
