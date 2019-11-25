9523a9f5f74cd20a17908d7bf5e982dfd2751201
Shawn Petros <shawn.petros@gmail.com>
2019-11-24 19:26:28 -0600

    update current job

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default ({ nm }) => (
+export default ({ gmr }) => (
   <div className='hero'>
     <div id='avatar' />
     <h1>Shawn Petros</h1>
     <h3 className='tagline'>philomath; father, husband, engineer</h3>
-    <p className='summary'>javascript and cloud guy...currently being awesome at <a href={nm} className='nm'>Northwestern Mutual</a></p>
+    <p className='summary'>javascript and cloud guy...currently leading frontend at <a href={gmr} className='gmr'>GMR Marketing</a></p>
   </div>
 )
