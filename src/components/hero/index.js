e1d2081adeac63fb53a0acebd22cab481261a564
Shawn Petros <shawn.petros@gmail.com>
2020-02-17 13:52:31 -0600

    meh

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -5,6 +5,6 @@ export default ({ tg }) => (
     <div id='avatar' />
     <h1>Shawn Petros</h1>
     <h3 className='tagline'>philomath; father, husband, engineer</h3>
-    <p className='summary'>javascript and cloud guy...currently architecting and solutioning at <a href={tg} className='tg'>TESCHGlobal</a></p>
+    <section className='summary'><p>javascript and cloud guy...</p><p>currently architecting and solutioning at <a href={tg} className='tg'>TESCHGlobal</a></p></section>
   </div>
 )
