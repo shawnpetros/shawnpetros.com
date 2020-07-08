71ee88b5cbfa00147618ac4ed7a362f36559e42c
Shawn Petros <shawn.petros@gmail.com>
2020-07-07 20:52:36 -0500

    Update index.js

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default ({ tg }) => (
+export default ({ amfam }) => (
   <div className='hero'>
     <div id='avatar' />
     <h1>Shawn Petros</h1>
     <h3 className='tagline'>philomath; father, husband, engineer</h3>
-    <section className='summary'><p>javascript and cloud guy...</p><p>currently architecting and solutioning at <a href={tg} className='tg'>TESCHGlobal</a></p></section>
+    <section className='summary'><p>javascript and cloud guy...</p><p>currently code wrangling at <a href={amfam} className='amfam'>American Family Insurance</a></p></section>
   </div>
 )
